be8722f3bda8fc10657141798267dcab5d852bc1
Tarekswd <flameshadowtarek@gmail.com>
2026-02-10 06:18:10 +0200

    Update Stuff

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -128,8 +128,10 @@ export const Projects = () => {
         {/* View All CTA */}
         <div className="text-center mt-12 animate-fade-in animation-delay-500">
           <AnimatedBorderButton>
+            <a href="https://github.com/Tarekswd?tab=repositories" target="_blank">
             View All Projects
             <ArrowUpRight className="w-5 h-5" />
+            </a>
           </AnimatedBorderButton>
         </div>
       </div>
